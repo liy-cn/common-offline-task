0fe7777a862ab58f44d0c95256b7de7c062c4cf7
liy <liyue20@jd.com>
2021-10-26 17:59:39 +0800

    优化导出

--- common-offline-task-web/src/main/java/org/storm/commons/commonofflinetaskweb/CommonOfflineTaskWebApplication.java
+++ common-offline-task-web/src/main/java/org/storm/commons/commonofflinetaskweb/CommonOfflineTaskWebApplication.java
@@ -1,11 +1,7 @@
 package org.storm.commons.commonofflinetaskweb;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.storm.commons.offlinetask.MyApi;
-
-import javax.annotation.Resource;
 
 @SpringBootApplication
 public class CommonOfflineTaskWebApplication {
